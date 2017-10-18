874566a057b12ebe1cc941d45309e1c0ef86156b
Torin Nguyen <torinnguyen@gmail.com>
2017-10-18 13:57:06 +0700

    Add verbose error messages to Current Volume node. Lots of cleaning up.

--- sonos/sonos-config.js
+++ sonos/sonos-config.js
@@ -1,8 +1,8 @@
 module.exports = function(RED) {
     function SonosPlayerNode(config) {
-        RED.nodes.createNode(this, config);
-        this.ipaddress = config.ipaddress;
-        this.port      = config.port;
+      RED.nodes.createNode(this, config);
+      this.ipaddress = config.ipaddress;
+      this.port      = config.port;
     }
 
     RED.nodes.registerType("sonos-config", SonosPlayerNode);
--- sonos/sonos-currentvol.js
+++ sonos/sonos-currentvol.js
@@ -6,27 +6,47 @@ module.exports = function(RED) {
 
     function Node(n) {
       
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
+		var node = this;
 		
 		var playnode = RED.nodes.getNode(n.playnode); 
-		this.client = new sonos.Sonos(playnode.ipaddress);
-		
-		var node = this;
-        
-        this.on('input', function (msg) {            
+		if (playnode === null || playnode === undefined) {
+        	node.status({fill:"red", shape:"ring", text:"please select a config node"});
+        	return;
+        }
+
+		node.client = new sonos.Sonos(playnode.ipaddress);
+		if (node.client === null || node.client === undefined) {
+        	node.status({fill:"red", shape:"dot", text:"node.client is null"});
+        	return;
+        }
+
+        node.status({});
+        node.on('input', function (msg) {            
             var payload = typeof msg.payload === 'object' ? msg.payload : {};
+
+            if (node.client === null || node.client === undefined) {
+            	node.status({fill:"red", shape:"dot", text:"node.client is null"});
+            	return;
+            }
 			
 			node.client.getVolume(function (err, volume) {
-				msg.payload = volume;
-				node.send(msg);
 				if (err) {
-					node.log(JSON.stringify(err));
+					node.error(JSON.stringify(err));
+					node.status({fill:"red", shape:"dot", text:"failed to retrieve volume"});
+					return;
 				}
-				node.log(JSON.stringify(volume));
-			});
+				if (volume === null || volume === undefined) {
+					node.status({fill:"red", shape:"dot", text:"invalid volume retrieved"});
+					return;	
+				}
 
+				msg.payload = volume;
+				msg.normalized = volume / 100.0;
+				node.send(msg);
+				node.status({fill:"green", shape:"dot", text:"vol: " + volume});
+			});
 		});
-		
 	}
 
     RED.nodes.registerType('sonos-currentvol', Node);
