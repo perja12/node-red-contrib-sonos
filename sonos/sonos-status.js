c1f76b0cc3ad42abd02cbdb461d3e20dea5b341e
Torin Nguyen <torinnguyen@gmail.com>
2017-10-19 22:53:27 +0700

    Added current playing state & mute status. Update node info.

--- sonos/sonos-status.js
+++ sonos/sonos-status.js
@@ -52,7 +52,7 @@ module.exports = function(RED) {
 				trackObj.albumArtURL = "http://" + ipaddress + ":" + port + trackObj.albumArtURI;
 			}
 			
-			//output 2
+			//Output data
 			msg.payload = trackObj.title;
 			msg.track = trackObj;
 			node.send([null, msg]);
@@ -82,23 +82,80 @@ module.exports = function(RED) {
 			}
 			if (volume < 0 || volume > 100) {
 				node.status({fill:"red", shape:"dot", text:"invalid volume range retrieved"});
-				return;	
+				return;
 			}
 
-			//output 1
+			//Output data
 			msg.volume = volume;
 			msg.normalized_volume = volume / 100.0;
-			node.send(msg);
 			
-			//volume + current track
-			var text = "vol:" + volume;
-			if (msg.payload)
-				text += " • " + msg.payload;
-			node.status({fill:"green", shape:"dot", text:text});
+			getSonosCurrentState(node, msg, ipaddress);
 		});
 	}
 
+	function getSonosCurrentState(node, msg, ipaddress) 
+	{
+		var sonos = require('sonos');
+		var client = new sonos.Sonos(ipaddress);
+		if (client === null || client === undefined) {
+			node.status({fill:"red", shape:"dot", text:"sonos client is null"});
+			return;
+		}
 
+		client.getCurrentState(function (err, state) {
+			if (err) {
+				node.error(JSON.stringify(err));
+				node.status({fill:"red", shape:"dot", text:"failed to retrieve current state"});
+				return;
+			}
+			if (state === null || state === undefined) {
+				node.status({fill:"red", shape:"dot", text:"invalid current state retrieved"});
+				return;	
+			}
+
+			//Output data
+			msg.state = state;
+
+			getSonosMuted(node, msg, ipaddress);
+		});
+	}
+
+	function getSonosMuted(node, msg, ipaddress) 
+	{
+		var sonos = require('sonos');
+		var client = new sonos.Sonos(ipaddress);
+		if (client === null || client === undefined) {
+			node.status({fill:"red", shape:"dot", text:"sonos client is null"});
+			return;
+		}
+
+		client.getMuted(function (err, muted) {
+			if (err) {
+				node.error(JSON.stringify(err));
+				node.status({fill:"red", shape:"dot", text:"failed to retrieve Mute status"});
+				return;
+			}
+			if (muted === null || muted === undefined) {
+				node.status({fill:"red", shape:"dot", text:"invalid Mute status retrieved"});
+				return;	
+			}
+
+			//volume + current track
+			var text = "";
+			if (muted)
+				text += "muted • ";
+			text += "vol:" + msg.volume;
+			if (msg.payload)
+				text += " • " + msg.payload;
+			node.status({fill:"green", shape:"dot", text:text});
+			
+			//Output data
+			msg.muted = muted;
+
+			//Send output
+			node.send(msg);
+		});
+	}
 	
 	RED.nodes.registerType('better-sonos-status', Node);
 };
